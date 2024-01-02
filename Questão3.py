7d06e6f0b72c91650c8d072d8b1a89b97acdac38
Matheus Henrique Martins Matozinho <matheus2709br@gmail.com>
2024-01-02 18:51:43 -0300

    Update Questão3.py

--- Questão3.py
+++ Questão3.py
@@ -1,4 +1,8 @@
 """
+Descrição: Questão 3 da Prova de conhecimentos técnicos - Estágio TCX
+Elaborado por: Matheus Henrique Martins Matozinho
+
+Resposta:   
 QUESTÃO 3)
 O OpenCV, uma biblioteca de visão computacional amplamente utilizada, emprega diversas estruturas de dados para representar imagens e vídeos. Uma das principais é a matriz NumPy, uma estrutura eficiente para manipulação de dados multidimensionais. A escolha entre uma matriz NumPy e estruturas específicas do OpenCV depende do contexto e dos requisitos da aplicação.
 A matriz NumPy é flexível e amplamente suportada, sendo integrada de forma transparente ao OpenCV. No entanto, em situações onde a eficiência é crucial, as estruturas nativas do OpenCV, como cv::Mat para imagens e cv::VideoCapture para vídeos, podem ser mais eficientes em termos de desempenho. Essas estruturas são otimizadas para operações específicas de processamento de imagem e vídeo, aproveitando otimizações de baixo nível.
